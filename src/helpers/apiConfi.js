19e79e52c4e9a7d889e9006138c75e3fc26fe4a2
Ale Bullo <bullorinia@gmail.com>
2022-04-30 17:56:07 -0300

    gitIgnore

--- src/helpers/apiConfi.js
+++ src/helpers/apiConfi.js
@@ -1,10 +1,5 @@
 const apiAuth = {
     method: 'GET',
-    // params: {league: '1', season: '2022'},
-    headers: {
-      'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com',
-      'X-RapidAPI-Key': 'be6ab61530msh0825cb606102ea6p1f1cd3jsne7000885330f'
-    }
   };
 
 const APIROUTE = 'https://api-football-v1.p.rapidapi.com/v3'
